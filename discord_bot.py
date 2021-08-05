3362246e331327d12cd50bc088f04adab21979bd
goguma9071 <whjhkkhjl@gmail.com>
2021-08-05 18:11:08 +0900

    daqewbot9

---
+++ discord_bot.py
@@ -0,0 +1,69 @@
+import discord
+import asyncio
+import random
+
+client = discord.Client()
+
+token = "ODY5NDE4MDA5NjQ1Njk1MDE2.YP96ng.1wDYekIXPRl6NAcqDgFq2N9t8QI"
+
+@client.event
+async def on_ready():
+    print(client.user.name)
+    print("사용법: !dq [명령어]")
+    game = discord.Game("!dq 도와줘")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+
+@client.event
+async def on_message(message):
+    if message.content == "!dq 고구마는":
+        await message.channel.send("꼬3븅신")
+    if message.content == "!dq 다큐는":
+        await message.channel.send("14cm")
+    if message.content == "!dq 곤잘은":
+        await message.channel.send("본인피셜7.5cm+화풀이심함+나한테 뻐큐100개 날린적있음")
+    if message.content == "!dq 로블은":
+        await message.channel.send("얜 대체 뭐야")
+    if message.content == "!dq 도와줘":
+        await message.channel.send("사용법: !dq [명령어] 명령어: 고구마는 다큐는 곤잘은 !Faitful는 나의 팬티색은? 오팬무 오늘의 곤잘 팬티는?")
+    if message.content == "!dq 오팬무":
+        await message.channel.send("주황")
+    if message.content == "!dq 나의 팬티색은?":
+        ran = random.randint(0,9)
+        if ran == 0:
+            d = "노랑"
+        if ran == 1:
+            d = "주황"
+        if ran == 2:
+            d = "파랭"
+        if ran == 3:
+            d = "하늘색"
+        if ran == 4:
+            d = "보라"
+        if ran == 5:
+            d = "검정"
+        if ran == 6:
+            d = "민트"
+        if ran == 7:
+            d = "민트색을 입은걸 보니 민초를 사1랑한다는 아주 장한뜻을 품었구나 ㅎㅎㅎㅎ녀석 장하기도 하지"
+        if ran == 8:
+            d = "빨강색을 입은걸 보니 열렬한 공산당원인가 보군ㅎㅎㅎㅎㅎ 중국의 위구르와 티베트가 돌립되갈 비네.https://www.youtube.com/watch?v=efDu-KVBz6w"
+        if ran == 9:
+            d = "빨강"
+        await message.channel.send(d)
+    if message.content == "!dq 오늘의 곤잘 팬티는?":
+        ran = random.randint(0,1)
+        if ran == 0:
+            d = "노팬티"
+        if ran == 1:
+            d = "팬티입음"
+        await message.channel.send(d)
+    if message.content == "!dq 집시는":
+        await message.channel.send("씹덕")
+
+
+
+
+client.run(token)
+
