ba041db337f6682eb3c9eb9480d31d77a76a3913
goguma9071 <whjhkkhjl@gmail.com>
2021-08-10 02:37:26 +0900

    daqewbot9

--- discord_bot.py
+++ discord_bot.py
@@ -4,7 +4,7 @@ import random
 
 client = discord.Client()
 
-token = "ODY5NDE4MDA5NjQ1Njk1MDE2.YP96ng.1wDYekIXPRl6NAcqDgFq2N9t8QI"
+token = "ODY5NDE4MDA5NjQ1Njk1MDE2.YP96ng.fe2SQhLHFIYmEhl63ZL4ED7H3Vs"
 
 @client.event
 async def on_ready():
@@ -24,11 +24,11 @@ async def on_message(message):
     if message.content == "!dq 곤잘은":
         await message.channel.send("본인피셜7.5cm+화풀이심함+나한테 뻐큐100개 날린적있음")
     if message.content == "!dq 로블은":
-        await message.channel.send("얜 대체 뭐야")
+        await message.channel.send("Идиот Отаку")
     if message.content == "!dq 도와줘":
         await message.channel.send("사용법: !dq [명령어] 명령어: 고구마는 다큐는 곤잘은 !Faitful는 나의 팬티색은? 오팬무 오늘의 곤잘 팬티는?")
     if message.content == "!dq 오팬무":
-        await message.channel.send("주황")
+        await message.channel.send("연하늘")
     if message.content == "!dq 나의 팬티색은?":
         ran = random.randint(0,9)
         if ran == 0:
@@ -48,7 +48,7 @@ async def on_message(message):
         if ran == 7:
             d = "민트색을 입은걸 보니 민초를 사1랑한다는 아주 장한뜻을 품었구나 ㅎㅎㅎㅎ녀석 장하기도 하지"
         if ran == 8:
-            d = "빨강색을 입은걸 보니 열렬한 공산당원인가 보군ㅎㅎㅎㅎㅎ 중국의 위구르와 티베트가 돌립되갈 비네.https://www.youtube.com/watch?v=efDu-KVBz6w"
+            d = "빨강색을 입은걸 보니 열렬한 공산당원인가 보군ㅎㅎㅎㅎㅎ 중국의 위구르와 티베트가 독립되길 비네.https://www.youtube.com/watch?v=efDu-KVBz6w"
         if ran == 9:
             d = "빨강"
         await message.channel.send(d)
@@ -61,7 +61,17 @@ async def on_message(message):
         await message.channel.send(d)
     if message.content == "!dq 집시는":
         await message.channel.send("씹덕")
-
+    if message.content == "!dq 오늘의 운세":
+        ran = random.randint(0,3)
+        if ran == 0:
+            d = "길가다가 벼락맞음"
+        if ran == 1:
+            d = "니 하고 있는거 다 망함"
+        if ran == 2:
+            d = "탈모 생김+여친없음"
+        if ran == 3:
+            d = "pvp 10연패함"
+        await message.channle.send(d)
 
 
 
